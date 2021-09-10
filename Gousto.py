5e7ae3a0c8c7565c2f4989c9505e19fca7359768
luigi.cogliani <luigi.cogliani@gmail.com>
2021-09-10 21:25:38 +0100

    changes

--- Gousto.py
+++ Gousto.py
@@ -2,21 +2,7 @@ import streamlit as st
 import pandas as pd
 import smtplib
 
-st.write("""# Gousto Grocery List Generator""")
-
-df = pd.read_csv('df.csv')
-
-st.sidebar.multiselect('Choose recipies',df['name'].unique()) 
-
-df_ = df.groupby('ing_name', as_index=False)['ing_q'].sum()
-
-st.write(df_)
-
-text = df_.to_string(header=False,index=False,justify='left')
-st.write(type(text))
-text = 'Shopping List\n' + text
-
-def send_email():
+def send_email(text):
     sender_email = 'stevio.bar@gmail.com'
     rec_email = 'house.cogliani@gmail.com'
     password = 'Luigifox7'
@@ -29,4 +15,21 @@ def send_email():
     server.ehlo()
     server.sendmail(sender_email,rec_email,message)
 
-st.button('Send!',on_click=send_email)
+
+
+st.write("""# Gousto Grocery List Generator""")
+
+df = pd.read_csv('df.csv')
+
+filter = st.sidebar.multiselect('Choose recipies',df['name'].unique()) 
+df_ = df[df['name'].isin(filter)]
+df_ = df.groupby('ing_name', as_index=False)['ing_q'].sum()
+
+text = df_.to_string(header=False,index=False,justify='left')
+text = 'Shopping List\n' + text
+
+st.button('Send!',on_click=send_email(text))
+st.write(df_)
+
+
+
