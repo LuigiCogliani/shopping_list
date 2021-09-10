a78a0db0c5e88ba1ec6e40d46b497cdcefd91d34
luigi.cogliani <luigi.cogliani@gmail.com>
2021-09-10 22:34:15 +0100

    it should work now

--- Gousto.py
+++ Gousto.py
@@ -8,34 +8,36 @@ df = pd.read_csv('df.csv')
 
 filter = st.sidebar.multiselect('Choose recipies',df['name'].unique()) 
 df_ = df[df['name'].isin(filter)]
-df_ = df.groupby('ing_name', as_index=False)['ing_q'].sum()
+df_ = df_.groupby('ing_name', as_index=False)['ing_q'].sum()
 
 text = df_.to_string(header=False,index=False,justify='left')
-text = 'Shopping List\n' + text
+
 
 #yandex mail settings
 #https://www.lifewire.com/what-are-the-yandex-mail-smtp-settings-1171304
-def send_email(text):
+# def send_email(text):
     
-    rec_email = 'house.cogliani@gmail.com'
-    message = text
+#     rec_email = 'house.cogliani@gmail.com'
+#     message = text
 
-    sender_email = 'stevio.bar@gmail.com'
-    password = 'Luigifox7'
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    #server.ehlo()
-    server.starttls()
-    server.login(sender_email,password)
-    #server.ehlo()
-    server.sendmail(sender_email,rec_email,message)
-    # sender_email = 'hosue.cogliani@yandex.com'
-    # password = 'B0tcogliani!'
-    # server = smtplib.SMTP('smtp.yandex.com', 465)
-    # server.login(sender_email,password)
-    # server.sendmail(sender_email,rec_email,message)
+#     sender_email = 'stevio.bar@gmail.com'
+#     password = 'Luigifox7'
+#     server = smtplib.SMTP('smtp.gmail.com', 587)
+#     #server.ehlo()
+#     server.starttls()
+#     server.login(sender_email,password)
+#     #server.ehlo()
+#     server.sendmail(sender_email,rec_email,message)
+#     # sender_email = 'hosue.cogliani@yandex.com'
+#     # password = 'B0tcogliani!'
+#     # server = smtplib.SMTP('smtp.yandex.com', 465)
+#     # server.login(sender_email,password)
+#     # server.sendmail(sender_email,rec_email,message)
+def shopping_list():
+    for line in text.splitlines():
+        st.checkbox(line)
 
-st.button('Send!',on_click=send_email(text))
-st.write(df_)
-
+if st.button('Generate!'):
+    shopping_list()
 
 
