5c355b27a6118b436193bc8e54f5c6f68c2be0b0
luigi.cogliani <luigi.cogliani@gmail.com>
2021-09-10 22:47:54 +0100

    ok now it should work

--- Gousto.py
+++ Gousto.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import smtplib
+
 
 st.write("""# Gousto Grocery List Generator""")
 
@@ -12,32 +12,8 @@ df_ = df_.groupby('ing_name', as_index=False)['ing_q'].sum()
 
 text = df_.to_string(header=False,index=False,justify='left')
 
-
-#yandex mail settings
-#https://www.lifewire.com/what-are-the-yandex-mail-smtp-settings-1171304
-# def send_email(text):
-    
-#     rec_email = 'house.cogliani@gmail.com'
-#     message = text
-
-#     sender_email = 'stevio.bar@gmail.com'
-#     password = 'Luigifox7'
-#     server = smtplib.SMTP('smtp.gmail.com', 587)
-#     #server.ehlo()
-#     server.starttls()
-#     server.login(sender_email,password)
-#     #server.ehlo()
-#     server.sendmail(sender_email,rec_email,message)
-#     # sender_email = 'hosue.cogliani@yandex.com'
-#     # password = 'B0tcogliani!'
-#     # server = smtplib.SMTP('smtp.yandex.com', 465)
-#     # server.login(sender_email,password)
-#     # server.sendmail(sender_email,rec_email,message)
-def shopping_list():
-    for line in text.splitlines():
+for line in text.splitlines():
         st.checkbox(line)
 
-if st.button('Generate!'):
-    shopping_list()
 
 
